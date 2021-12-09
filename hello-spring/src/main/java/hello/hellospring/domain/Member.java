d3235b33eed843930e635125733af008006c10e2
logan <gnlee95@gmail.com>
2021-12-10 01:38:56 +0900

    Update create member domain and repository

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,2 +1,23 @@
-package hello.hellospring.domain;public class Member {
+package hello.hellospring.domain;
+
+public class Member {
+
+    private Long id;  // 로그인 아이디가 아닌 시스템이 관리하는 아이디
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
